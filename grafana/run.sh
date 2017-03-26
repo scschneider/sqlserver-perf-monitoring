bcdee9139293dcc9a1624a1c1bcbf2a47224ee4c
scschneider <stschn@microsoft.com>
2017-03-26 12:34:07 -0700

    Update run.sh

--- grafana/run.sh
+++ grafana/run.sh
@@ -1,1 +1,2 @@
-sudo docker run --net=host -d --name grafana scschneider/sqlserver-perf-monitoring-grafana
+#We use the grafana image that Grafana Labs provides http://docs.grafana.org/installation/docker/
+sudo docker run -d -p 3000:3000 --name grafana grafana/grafana
